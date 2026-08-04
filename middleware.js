aee5c0a2576a5bf618d7e6d08f56b4920fd5837f
tararais11 <tararais11@gmail.com>
2026-08-04 10:19:44 +0900

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -8,6 +8,9 @@ export async function middleware(request) {
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
+      cookieOptions: {
+        maxAge: undefined,
+      },
       cookies: {
         getAll() {
           return request.cookies.getAll();
@@ -16,7 +19,7 @@ export async function middleware(request) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
           response = NextResponse.next({ request });
           cookiesToSet.forEach(({ name, value, options }) =>
-            response.cookies.set(name, value, options)
+            response.cookies.set(name, value, { ...options, maxAge: undefined, expires: undefined })
           );
         },
       },
